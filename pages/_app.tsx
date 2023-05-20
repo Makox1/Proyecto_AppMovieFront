ea64c3d5261c33bcc21cb0e83a2a4d516624e8ab
Diego Barros <diego.barros@alumnos.ucn.cl>
2023-05-20 03:32:19 -0400

    Eliminacion de codigo innecesario

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,6 @@
-// _app.tsx
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ApolloProvider } from '@apollo/client';
-import { apolloClient } from './apolloClient';
 
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp;
